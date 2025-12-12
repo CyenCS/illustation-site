ee4748572d022c51967e2b78906a450a1702b0c5
CyenCS <chooyencs@gmail.com>
2025-12-13 04:31:54 +0800

    Developing Profile

--- src/Script/ArtworkHooks.js
+++ src/Script/ArtworkHooks.js
@@ -3,10 +3,13 @@ import axios from "axios";
 
 const APIURL = process.env.REACT_APP_API_URL || "https://illustation-site.onrender.com";
 
+//Custom hook to fetch artwork posts based on search, pagination, and profile ID
+
 export default function ArtworkHooks({search, currentPage = 1, profileid=null}) {
     const [posts, setPosts] = useState([]);
     const [total, setTotal] = useState(0);
     const [maxPage, setMaxPage] = useState(1);
+    const [artistname, setArtistname] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     // const [selectedPage ,setCurrentPage] = useState(currentPage);
@@ -27,11 +30,16 @@ export default function ArtworkHooks({search, currentPage = 1, profileid=null}) 
                   setPosts(res.data.posts);
                   setTotal(res.data.total);
                   setMaxPage(res.data.maxpage);
+                  if (profileid) {
+                    const username = res.data.posts[0]?.username;
+                    setArtistname(username); // 👈 capture username from backend
+                  }
                   setError(null);
               } else{
                 setPosts([]);
                 setTotal(0);
                 setMaxPage(1);
+                setArtistname("");
               }
             }).catch((err) => {
               setPosts([]);
@@ -44,5 +52,5 @@ export default function ArtworkHooks({search, currentPage = 1, profileid=null}) 
         //   fetchPosts(search, currentPage);
         },[search, currentPage, profileid]);
 
-    return { posts, total, maxPage, loading, error };
+    return { posts, total, artistname, maxPage, loading, error };
 }
