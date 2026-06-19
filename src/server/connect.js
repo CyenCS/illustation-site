89857146adca5c1cb32d1f017aeb52a79100410f
CyenCS <chooyencs@gmail.com>
2026-06-19 14:15:11 +0800

    Backend Bug Fixes

--- src/server/connect.js
+++ src/server/connect.js
@@ -1,10 +1,5 @@
 const mysql = require('mysql2');
 
-if (process.env.NODE_ENV !== 'production') {
-  const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '..' , '.env') });
-}
-
 const pool = mysql.createPool({
 host: process.env.DBHOST,
 user: process.env.DBUSER,
